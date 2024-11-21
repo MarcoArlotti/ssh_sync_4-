c3db758a02f101d97e2aa9f4dd2e136b4d06b4c6
MarcoArlotti <marco.arlotti@studenti.isissgobetti.it>
2024-11-21 19:13:34 +0100

    fix

--- arlotti_es13/arlotti_esercizio_python_numero_13.py
+++ arlotti_es13/arlotti_esercizio_python_numero_13.py
@@ -4,20 +4,22 @@ class Casa:
         self._proprietario = proprietario
         self.stanze = []
 
-    #def aggiungi_stanza(self,stanza):
-    #    self.stanze.append(stanza)
+    def aggiungi_stanza(self,stanza):
+        lista_stanze = self.stanze
+        lista_stanze.append(stanza)
+        self.stanze = lista_stanze
 #
     @property
     def indirizzo(self):
-        return self.indirizzo
+        return self._indirizzo
     @indirizzo.setter
     def indirizzo(self,indirizzo):
         if type(indirizzo) == str:
-            self.indirizzo = indirizzo
+            self._indirizzo = indirizzo
     
     @property
     def proprietario(self):
-        return self.proprietario
+        return self._proprietario
     @proprietario.setter
     def proprietario(self,proprietario):
         if type(proprietario) == str:
@@ -30,19 +32,19 @@ class Stanza:
 
     @property
     def nome(self):
-        return self.nome
+        return self._nome
     @nome.setter
     def nome(self,nome):
-        if type(nome) == str:
-            self.nome = nome
+        if type(self._nome) == str:
+            self._nome = nome
     
     @property
     def superficie(self):
-        return self.superficie
+        return self._superficie
     @superficie.setter
-    def proprietario(self,superficie):
-        if type(superficie) == str:
-            self.superficie = superficie
+    def superficie(self,superficie):
+        if type(self._superficie) == str:
+            self._superficie = superficie
 
 # Creazione dell'istanza di Casa
 casa = Casa("Via delle Rose 15", "Maria Rossi")
@@ -53,11 +55,11 @@ cucina = Stanza("Cucina", 15)
 camera = Stanza("Camera da Letto", 20)
 
 # Aggiunta delle stanze alla casa
-#casa.aggiungi_stanza(soggiorno)
-#casa.aggiungi_stanza(cucina)
-#casa.aggiungi_stanza(camera)
+casa.aggiungi_stanza(soggiorno)
+casa.aggiungi_stanza(cucina)
+casa.aggiungi_stanza(camera)
 
-# Verifica dell'associazione, TODO ERRORE SELF.PROPRIETARIO 
+# Verifica dell'associazione
 print(f"Casa di {casa.proprietario} situata in {casa.indirizzo} contiene le seguenti stanze:")
 for stanza in casa.stanze:
     print(f"- {stanza.nome} ({stanza.superficie} mq)")
