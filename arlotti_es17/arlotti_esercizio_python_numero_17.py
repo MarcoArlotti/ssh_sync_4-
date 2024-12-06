97f0ffd8bb48fe0d1576e28dc29f7653b905e416
Marcoarlotti <marco.arlotii@studenti.isissgobetti.it>
2024-12-06 09:51:10 +0100

    fix

--- arlotti_es17/arlotti_esercizio_python_numero_17.py
+++ arlotti_es17/arlotti_esercizio_python_numero_17.py
@@ -1,21 +1,78 @@
-class Insegnanti:
-    def __init__(self,nome,cognome,strumento_musicale):
+class Persona:
+    def __init__(self, nome:str ,cognome:str):
         self._nome = nome
         self._cognome = cognome
-        self._strumento_musicale = strumento_musicale
-    def
 
-class Studenti(Insegnanti):
-    def __init__(self,nome,cognome):
+    @property
+    def nome(self):
+        return self._nome
+    @nome.setter
+    def nome(self,nome):
+        if type(nome) == str:
+            self._nome = nome
+
+    @property
+    def cognome(self):
+        return self._cognome
+    @cognome.setter
+    def cognome(self,cognome):
+        if type(cognome) == str:
+            self._cognome = cognome
+    
+class Studente(Persona):
+    def __init__(self, nome:str ,cognome:str):
         super().__init__(nome,cognome)
-        self._lista_corsi = []
-    
+        self.corsi = []
+        self.insegnante = None
 
-class Corsi(Insegnanti):
-    def __init__(self,nome,durata):
-        super().__init__(nome)
-        self._durata = durata
+    def set_insegnante(self,insegnate_nuovo):
+        self.insegnante = insegnate_nuovo
     
+    def iscrivi_corso(self,corso):
+        self.corsi.append(corso)
+        # corso.studenti.append(self)
+        print(f"corso.studenti {corso.studenti}")
+        corso.studenti.append("test")
+        print(f"corso.studenti {corso.studenti}")
+        
+
+class Insegnante(Persona):
+    def __init__(self,nome,cognome,strumento):
+        super().__init__(nome,cognome)
+        self._strumento = strumento
+        self.studenti = []
+
+    @property
+    def strumento(self):
+        return self._strumento
+    @strumento.setter
+    def strumento(self,strumento):
+        if type(strumento) == str:
+            self._strumento = strumento
+
+class Corso:
+    def __init__(self,nome,durata):
+        self._durata = durata
+        self._nome = nome
+        self.studenti = []
+
+    @property
+    def nome(self):
+        return self._nome
+    @nome.setter
+    def nome(self,nome):
+        if type(nome) == str:
+            self._nome = nome
+
+    @property
+    def durata(self):
+        return self._durata
+    @durata.setter
+    def durata(self,durata):
+        if type(durata) == str:
+            self._durata = durata
+
+
 
 def main():
     # Creazione degli insegnanti
@@ -40,8 +97,9 @@ def main():
     studente2.iscrivi_corso(corso1)
 
     # Stampa delle informazioni
-    print(studente1)
-    print(studente2)
+    print(studente1.insegnante.nome)
+    for s in insegnante1.studenti:
+        print(s)
 
 if __name__ == "__main__":
     main()
