9dfd96cdddd45a15752a84a3117336756b3cd0c4
Marcoarlotti <marco.arlotii@studenti.isissgobetti.it>
2025-01-24 09:44:42 +0100

    fix

--- crea_cartelle.py
+++ crea_cartelle.py
@@ -1,6 +1,6 @@
 import os
 
-i = 24 #numero esercizio da preimpostare
+i = 25 #numero esercizio da preimpostare
 os.system(f"mkdir arlotti_es{i}") #crea cartella dell'esercizio
 with open(f'arlotti_es{i}/arlotti_esercizio_python_numero_{i}.py', 'w') as file_da_creare: #crea esercizio in python
     file_da_creare.write("")
