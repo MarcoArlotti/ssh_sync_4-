0c53a9b19d1e61b5a8361ae5c190c4fc5158b04b
Marcoarlotti <marco.arlotii@studenti.isissgobetti.it>
2025-03-24 08:35:28 +0100

    fix

--- arlotti_es32/arlotti_esercizio_python_numero_32.py
+++ arlotti_es32/arlotti_esercizio_python_numero_32.py
@@ -1,37 +1,95 @@
 class Video:
     def __init__(self,titolo,descrizione,url,durata,commenti):
-        pass
+        self.titolo = titolo
+        self.descrizione = descrizione
+        self.url = url
+        self.durata = durata
+        self.commenti = commenti
 
-    class Playlist:
-        def __init__(self,video,creatore):
-            self.nome = creatore.nome
+class Playlist:
+    def __init__(self,video,creatore,nome):
+        self.nome = nome
+        self.video = video
+        self.creatore = creatore
 
-    }
-    class Utente:
-        def __init__(self,nome,email,password,playlist,abbonamento):
-            pass
-        def crea_playlist(nome):
-            pass
-        def guarda_video(video):
-            pass
-        def aggiungi_video_playlist(video,playlist):
-            pass
-        def rimuovi_video_playlist(video,playlist):
-            pass
-        def cancella_playlist(playlist):
-            pass
-        def aggiungi_commento(video):
-            pass
-    }
-    class Commento:
-        def __init__(autore,commento,data_di_pubblicazione,video,canali_seguiti):
-            pass
+class Utente:
+    def __init__(self,nome,email,password,playlist,abbonamento):
+        self.nome = nome
+        self.email = email
+        self.password = password
+        self.playlist = playlist
+        self.abbonamento = abbonamento
+        self.playlists = []
+
+    def crea_playlist(self,nome):
+        try:
+            playlist = Playlist([],self,nome)
+            self.playlists.append(playlist)
+            return True
+        except:
+            return False
+
+    def guarda_video(self,video):
+        print(f"{self.nome} sta guardando {video.titolo}")
+
+    def aggiungi_video_playlist(self,video,playlist):
+        try:
+            playlist.append(video)
+            self.playlists.update(playlist)
+            return True
+        except:
+            return False
+
+    def rimuovi_video_playlist(self,video,playlist):
+        try:
+            for video_ in playlist:
+                if video_.titolo == video.titolo:
+                    playlist.remove(video)
+                    self.playlists.update(playlist)
+                    return True
+            return False
+        except:
+            return False
+
+    def cancella_playlist(self,playlist):
+        try:
+            self.playlists.remove(playlist)
+            return True
+        except:
+            return False
+
+    def aggiungi_commento(self,video,commento):
+        try:
+            video.commenti.append(commento)
+            return True
+        except:
+            return False
         
-    class Piattaforma {
-    }
-    class Abbonamento {
-        -date inizio
-        -date fine
-        -str tier
-        -float prezzo %%??????
-    }
+
+
+
+class Commento:
+    def __init__(self,autore,commento,data_di_pubblicazione,video,canali_seguiti):
+        self.autore = autore
+        self.commento = commento
+        self.data_di_pubblicazione = data_di_pubblicazione
+        self.video = video
+        self.canali_seguiti = canali_seguiti
+
+class Piattaforma:
+    def __init__(self):
+        self.video = []
+
+    def aggiungi_video(self,video_):
+        try:
+            self.video.append(video_)
+            return True
+        except:
+            return False
+
+class Abbonamento:
+    def __init__(self,inizio,fine,tier,prezzo):
+        self.inizio = inizio
+        self.fine = fine
+        self.tier = tier
+        self.prezzo = prezzo
